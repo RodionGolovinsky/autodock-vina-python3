d59617f7c5364a9e8a4132b7e42e15b8a49c0ef4
asya orlova <asya-1610@yandex.ru>
2024-09-18 20:54:23 +0300

    optimized labox and added reference ligand example

--- src/docking.py
+++ src/docking.py
@@ -5,28 +5,31 @@ sys.path.append('..')
 from utils.paths import get_project_path
 
 import numpy as np 
-from utils.prepare_receptor import prepare_pdb, get_center_of_protein, get_bounding_box, get_box_center_and_size
+from utils.prepare_receptor import prepare_pdb, get_center_of_protein, get_bounding_box, LaBOX
 from utils.prepare_ligand import preprocess_ligand
 
 import time
 
 # Ligand SMILES (Apigenin)
-ligand_smiles = 'O=C(O)c1cc(Nc2nccc(C3CCOC3)n2)cc(-c2nnco2)c1'
+ligand_smiles = 'c1ccc2c(c1)c(c[nH]2)C[C@@H]3C(=O)Nc4c(cccn4)N3'
 # Path to PDB for receptor (A2B1) 
-path_receptor_pdb = os.path.join(get_project_path(), 'data', '3fqs_protein.pdb')
-ligand_path = os.path.join(get_project_path(), 'data', '3fqs_reference.pdb')
+path_receptor_pdb = os.path.join(get_project_path(), 'data', '4j1r.pdb')
+ligand_path = os.path.join(get_project_path(), 'data', '4j1r_reference.pdb')
 
 def get_docking_score(pdb_fie: str, smiles_ligand: str, box_search=False, ligand_file=None, n_poses=100, max_steps=100, padding=10):
-    #start = time.time()
+    # start = time.time()
     preprocess_ligand(smiles_ligand)
     prepare_pdb(path_to_receptor=pdb_fie)
 
     v = Vina(sf_name='vina')
 
+    ext = os.path.splitext(ligand_file)[-1]
+    data = open(ligand_file, 'r').readlines()
+
     if box_search:
         center = get_center_of_protein(path_receptor_pdb)
     else:
-        center = get_box_center_and_size(ligand_file)[0]
+        center = LaBOX(data, ext)[0]
 
 
     path_receptor = os.path.join(os.getcwd(), 'receptor.pdbqt')
@@ -39,17 +42,16 @@ def get_docking_score(pdb_fie: str, smiles_ligand: str, box_search=False, ligand
         box_size = get_bounding_box(path_receptor_pdb)
         box_size = [size + padding for size in box_size]
     else:
-        box_size = get_box_center_and_size(ligand_file)[1]
-        box_size = [size + padding for size in box_size]
+        box_size = LaBOX(data, ext)[1]
 
 
     v.compute_vina_maps(center=center, box_size=box_size)
 
-    v.dock(exhaustiveness=32, n_poses=n_poses)
+    v.dock(exhaustiveness=8, n_poses=n_poses)
 
     energy_minimized = v.optimize(max_steps=max_steps)
-    #end = time.time()
-    #print(end-start)
+    # end = time.time()
+    # print(end-start)
     print('Score after minimization : %.3f (kcal/mol)' % energy_minimized[0])
     os.system(f"rm {path_ligand} {path_receptor}")
 
--- src/utils/prepare_receptor.py
+++ src/utils/prepare_receptor.py
@@ -3,6 +3,7 @@ import subprocess
 from Bio.PDB import PDBParser
 import numpy as np
 import os
+import statistics
 
 def prepare_pdb(path_to_receptor):
     cmd = f"obabel {path_to_receptor} -xr -O {os.path.join(os.getcwd(), 'receptor.pdbqt')}"
@@ -47,13 +48,40 @@ def get_bounding_box(pdb_path):
     
     return box_size.tolist()
 
-def get_box_center_and_size(ligand_filename):
-    """
-    Calculates the center and size of the binding box based on the reference ligand coordinates.
-    """
-    run_line = 'python LaBOX.py -l {} -c'.format(ligand_filename)
-    result = subprocess.run(run_line.split(), capture_output=True, text=True)
-    print(result)
-    result_center = [float(i) for i in result.stdout.split('\n')[0].split(' ')]
-    result_size = [float(i) for i in result.stdout.split('\n')[1].split(' ')]
-    return result_center, result_size
+def get_coordinates(data, ext):
+    if ext == '.mol2':
+        commence = next(n for n, line in enumerate(data) if line.strip() != '' and line.split()[0] == '@<TRIPOS>ATOM') + 1
+        conclude = next(n for n, line in enumerate(data) if line.strip() != '' and line.split()[0] == '@<TRIPOS>BOND')
+        atoms = [line.split()[1] for line in data[commence:conclude]]
+        coord = [list(map(float, line.split()[2:5])) for line in data[commence:conclude]]
+
+    if ext == '.sdf':
+        blank = int(next(n for n, line in enumerate(data) if line.split() == []))
+        atoms = int(data[blank + 1].split()[0])
+        commence = blank + 2
+        conclude = commence + atoms
+        coord = [list(map(float, line.split()[:3])) for line in data[commence:conclude]]
+
+    if ext in ('.pdb', '.pdbqt'):
+        coord = [[float(line[31:38]), float(line[39:46]), float(line[47:54])] for line in data if line.split()[0] in ('ATOM', 'HETATM')]
+
+    xcoor, ycoor, zcoor = zip(*coord)
+    return [list(xcoor), list(ycoor), list(zcoor)]
+
+def min_max(coord):
+    return [min(coord), max(coord)]
+
+def center_XYZ(coord_range):
+    return round(statistics.mean(coord_range), 3)
+
+def length_WHD(coord_range, scale):
+    return round(abs(coord_range[0] - coord_range[1]) * scale, 3)
+
+def LaBOX(data, ext, scale=2):
+    COOR = get_coordinates(data, ext)
+    X,Y,Z = COOR
+    ranges = [min_max(X), min_max(Y), min_max(Z)]
+    center = [center_XYZ(ranges[0]), center_XYZ(ranges[1]), center_XYZ(ranges[2])]
+    bxsize = [length_WHD(ranges[0], scale), length_WHD(ranges[1], scale), length_WHD(ranges[2], scale)]
+    
+    return center, bxsize
